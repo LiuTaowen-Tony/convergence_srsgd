5fdb7bbd32614699ea51c33d38ecc2e32598ab2e
LiuTaowen Tony <tony_t_liu@outlook.com>
2024-06-03 01:58:47 +0100

    support batch size greater than dataset size

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,11 +3,14 @@
 git submodule update --recursive
 
 # Define the grid search parameters
-man_widths=(0 1 2 4 8 16 23)
-batch_sizes=(8 16 32 64 128)
+man_widths=(0 1 2)
+batch_sizes=(1 10 50 100 200 300 400 500 600 700)
 lrs=(0.03)
 round_mode="stochastic"
-experiment_name="mnist_mlp"
+experiment_name="large_batch_size_activation_quant_only"
+model="logistic"
+dataset="small_dataset"
+weight_man_width=23
 
 # Job count for controlling GPU assignment
 job_count=0
@@ -20,19 +23,18 @@ for batch_size in "${batch_sizes[@]}"; do
     # Loop for running each configuration 8 times
     for (( i=1; i<= 8; i++ )); do
       # Calculate GPU ID to use
-      gpu_id=$((job_count % 4))
+      gpu_id=$((job_count % 1 + 3))
 
       cmd="OMP_NUM_THREADS=8 \
       CUDA_VISIBLE_DEVICES=$gpu_id \
       python experiment.py \
       --experiment_name=$experiment_name
-      --model=mnist_mlp \
-      --dataset=mnist \
-      --steps=100_000 \
+      --model=$model \
+      --dataset=$dataset \
+      --steps=10_000 \
       --batch_size=$batch_size \
       --weight_man_width=$man_width \
       --act_man_width=$man_width \
-
       "
       
       # Run the command in the background
